51f6a4eeefd0d2ffd28330d502f2c4b2abafbb28
enbacon <bacon.erin@gmail.com>
2020-02-25 10:24:13 -0500

    Turn off eslint for this file only

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+
+/* eslint-disable */
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+})
