44e1af8ab26c3ea24834cbd013921c9e2e25bf88
enbacon <bacon.erin@gmail.com>
2020-02-25 16:45:16 -0500

    Add classNames to help with styling

--- src/Builder.js
+++ src/Builder.js
@@ -16,19 +16,19 @@ const Builder = () => {
     required: false
   })
 
-  const choicesLogic = () => {
-    const formattedChoices = field.choices.split('\n')
-    console.log('this is formatted choices', formattedChoices)
-  }
-
-  const handleSubmit = event => {
-    event.preventDefault()
-    console.log('this is fieldSaved', field)
-  }
+  let formattedChoices
+  // const choicesLogic = () => {
+  //   // convert choices string to an array of unique elements
+  //   formattedChoices = [...new Set(field.choices.split('\n'))]
+  //   console.log('this is formatted choices', formattedChoices)
+  //   console.log(2)
+  // }
 
   const handleChange = event => {
     event.persist()
+    console.log('formattedChoices in handleChange', formattedChoices)
     setField(field => ({ ...field, [event.target.name]: event.target.value }))
+    console.log(1)
   }
 
   const handleCheck = event => {
@@ -36,102 +36,118 @@ const Builder = () => {
     setField(field => ({ ...field, [event.target.name]: !field[event.target.name] }))
   }
 
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log('this is fieldSaved in handleSubmit', field)
+    console.log(3)
+  }
+
   return (
-    <div className="form">
-      <Container>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group as={Row} controlId="label">
-            <Col md={4}>
-              <Form.Label className="label">Label</Form.Label>
-            </Col>
-            <Col>
-              <Form.Control
-                required
-                type="text"
-                name="label"
-                placeholder="Cake Flavors"
-                onChange={handleChange}
-                md={7}
-              />
-            </Col>
-          </Form.Group>
+    <div>
+      <Container className="container">
+        <div className="form-field">
+          <h2>Field Builder</h2>
+          <Form className="form" onSubmit={handleSubmit}>
+            <Form.Group as={Row} controlId="label">
+              <Col md={4}>
+                <Form.Label className="label">Label</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control
+                  required
+                  type="text"
+                  name="label"
+                  placeholder="Cake Flavors"
+                  onChange={handleChange}
+                  value={field.label}
+                  md={7}
+                />
+              </Col>
+            </Form.Group>
 
-          <Form.Group as={Row} controlId="default" className="small-entry">
-            <Col md={4}>
-              <Form.Label>Default Value</Form.Label>
-            </Col>
-            <Col>
-              <Form.Control
-                type="text"
-                name="default"
-                placeholder="Red Velvet"
-                onChange={handleChange}
-                md={7} />
-            </Col>
-          </Form.Group>
+            <Form.Group as={Row} controlId="default" className="small-entry">
+              <Col md={4}>
+                <Form.Label className="label">Default Value</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control
+                  type="text"
+                  name="default"
+                  placeholder="Red Velvet"
+                  onChange={handleChange}
+                  value={field.default}
+                  md={7} />
+              </Col>
+            </Form.Group>
 
-          <Form.Group as={Row} controlId="choices">
-            <Col md={4}>
-              <Form.Label>Choices</Form.Label>
-            </Col>
-            <Col>
-              <Form.Control
-                as="textarea"
-                name="choices"
-                placeholder="Add Choices"
-                onChange={handleChange}
-                md={7}
-                rows="5" />
-            </Col>
-          </Form.Group>
+            <Form.Group as={Row} controlId="choices">
+              <Col md={4}>
+                <Form.Label className="label">Choices</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control
+                  as="textarea"
+                  className="text-area"
+                  name="choices"
+                  placeholder="Add Choices"
+                  onChange={handleChange}
+                  value={field.choices}
+                  md={7} />
+              </Col>
+            </Form.Group>
 
-          <Form.Group as={Row} controlId="alphabetical">
-            <Col md={4}>
-            </Col>
-            <Col>
-              <Form.Check
-                type="checkbox"
-                name="displayAlpha"
-                onChange={handleCheck}
-                checked={field.displayAlpha}
-                label="Display Alphabetically" />
-            </Col>
-          </Form.Group>
+            <Form.Group as={Row} controlId="alphabetical">
+              <Col md={4}>
+              </Col>
+              <Col>
+                <Form.Check
+                  type="checkbox"
+                  name="displayAlpha"
+                  onChange={handleCheck}
+                  checked={field.displayAlpha}
+                  label="Display Alphabetically" />
+              </Col>
+            </Form.Group>
 
-          <Form.Group as={Row} controlId="multiSelect">
-            <Col md={4}>
-            </Col>
-            <Col>
-              <Form.Check
-                type="checkbox"
-                name="multiSelect"
-                onChange={handleCheck}
-                checked={field.multiSelect}
-                label="Select Multiple Choices" />
-            </Col>
-          </Form.Group>
+            <Form.Group as={Row} controlId="multiSelect">
+              <Col md={4}>
+              </Col>
+              <Col>
+                <Form.Check
+                  type="checkbox"
+                  name="multiSelect"
+                  onChange={handleCheck}
+                  checked={field.multiSelect}
+                  label="Select Multiple Choices" />
+              </Col>
+            </Form.Group>
 
-          <Form.Group as={Row} controlId="required">
-            <Col md={4}>
-            </Col>
-            <Col>
-              <Form.Check
-                type="checkbox"
-                name="required"
-                onChange={handleCheck}
-                checked={field.required}
-                label="Required Field" />
-            </Col>
-          </Form.Group>
+            <Form.Group as={Row} controlId="required">
+              <Col md={4}>
+              </Col>
+              <Col>
+                <Form.Check
+                  type="checkbox"
+                  name="required"
+                  onChange={handleCheck}
+                  checked={field.required}
+                  label="Required Field"
+                  value={field.required} />
+              </Col>
+            </Form.Group>
 
-          <Col>
-            <Button variant="success" type="submit">Save Changes</Button>
-          </Col>
-          <Col>
-            <p>Or</p>
-          </Col>
-          <Button variant="outline-danger btn-sm" type="submit">Reset Form</Button>
-        </Form>
+            <Form.Group as={Row}>
+              <Col md={4}>
+              </Col>
+              <Col md={4}>
+                <Button className="save" variant="success" type="submit">Save Changes</Button>
+              </Col>
+              <Col md={4}>
+                <Button className="reset" variant="outline-danger" type="submit">Reset Form</Button>
+              </Col>
+            </Form.Group>
+          </Form>
+        </div>
       </Container>
 
     </div>
