a3be9ea8666ededf2129b00a5494daddadac27f8
enbacon <bacon.erin@gmail.com>
2020-10-05 12:00:57 -0400

    Clean up validations
    - Adjust spacing
    - Add comments

--- src/validations.js
+++ src/validations.js
@@ -1,23 +1,23 @@
-const MAX_CHOICES = 4
+const MAX_CHOICES = 50
 
 // Export function to allow it to be used in Builder.js
-// Making this custom validation reusable
-
+// Makes this custom validation reusable
 export function validate (choicesArray, maxCharacterLength) {
   // When validate is called, clear errors to avoid false notifications
   const errors = { duplicatesError: '', choicesError: '', lengthError: '' }
 
+  // Duplicates check
   const duplicates = findDuplicates(choicesArray)
   if (duplicates.length !== 0) {
     errors.duplicatesError = `* Duplicate choices are not allowed. Please remove the following duplicates: ${duplicates.join(', ')}`
   }
 
+  // Choice Limit check
   if (choicesArray.length > MAX_CHOICES) {
     errors.choicesError = `* You have entered ${choicesArray.length} choices (maximum of ${MAX_CHOICES} allowed). Please delete ${choicesArray.length - MAX_CHOICES} before saving. Note: Default Choice should not be deleted.`
-    // indexOf default choice
-    // if -1 additional notification, default choice may not be deleted
   }
 
+  // Character Limit check
   const longWords = findLongChoices(choicesArray, maxCharacterLength)
   if (longWords.length !== 0) {
     errors.lengthError = `* The following choices exceed the ${maxCharacterLength} character maximum: ${longWords.join(', ')}`
@@ -26,6 +26,7 @@ export function validate (choicesArray, maxCharacterLength) {
 }
 
 // Find duplicate entries
+// Return duplicate choices
 function findDuplicates (choicesArray) {
   const choicesArrayLowerCase = choicesArray.map(choice => choice.toLowerCase())
 
@@ -37,6 +38,7 @@ function findDuplicates (choicesArray) {
 }
 
 // Find choices that exceed the MAX_CHARACTER_LIMIT
+// Return choices that are too long
 function findLongChoices (choicesArray, maxCharacterLength) {
   const longWords = choicesArray.filter(choice => choice.length > maxCharacterLength)
   return longWords
